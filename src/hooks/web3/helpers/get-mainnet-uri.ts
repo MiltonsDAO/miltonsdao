592e195f3f1031fd4e23003e8362e14a4f698417
MiltonsDAO <miltonsdao@protonmail.com>
2022-05-08 14:52:57 +0800

    update

--- src/hooks/web3/helpers/get-mainnet-uri.ts
+++ src/hooks/web3/helpers/get-mainnet-uri.ts
@@ -1,6 +1,6 @@
 export const getMainnetURI = (mainnet): string => {
   if (mainnet == 56) {
-    return 'https://icy-weathered-violet.bsc.quiknode.pro/0617462be53bb10061e99025fa2cd12893fb6efb/'
+    return 'https://bsc-dataseed.binance.org/'
   } else if (mainnet == 97) {
     return 'https://data-seed-prebsc-1-s2.binance.org:8545/'
   } else if (mainnet == 31337){
